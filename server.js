209c09418e4928eef32d569c5b6c5a159a872745
Nayaab Akhtar <nayaabakhtar154@gmail.com>
2024-06-02 18:55:24 +0530

    server.js updated

--- server.js
+++ server.js
@@ -8,12 +8,17 @@ import categoryRoutes from "./routes/categoryRoutes.js"
 import productRoutes from './routes/productRoutes.js'
 import cors from "cors"
 import path from 'path'
+import { fileURLToPath } from 'url';
 
 //configure dotenv
 dotenv.config();
 
 //database config
 connectDB();
+
+//esmodule fix
+const __filename=fileURLToPath(import.meta.url)
+const __dirname=path.dirname(__filename)
 
 const app=express();
 
